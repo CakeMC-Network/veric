9a83771600ac3874042773a6e6ed46e0ba1a1904
u64lisa <106406816+u64lisa@users.noreply.github.com>
2025-02-14 16:57:35 +0100

    :sparkles: implemented publishing for use in diff. projects

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,84 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import java.net.URI
+import java.nio.charset.StandardCharsets
+import java.util.*
+
 plugins {
     kotlin("jvm") version "2.0.0"
 
     id("java")
+
+    id("maven-publish")
+    id("com.gradleup.shadow") version "8.3.0"
 }
 
 group = "net.cakemc.library"
-version = "1.0-SNAPSHOT"
+version = "0.0.0-develop"
+
+var repoProperties = Properties()
+var repoFile = file("/credentials.properties")
+if (repoFile.exists())
+    repoProperties.load(repoFile.inputStream())
+var repoUsername = repoProperties.getProperty("username", System.getenv("REPOSITORY_USERNAME"))
+var repoPassword = repoProperties.getProperty("password", System.getenv("REPOSITORY_PASSWORD"))
 
 repositories {
+    mavenLocal()
     mavenCentral()
+    maven {
+        name = "cakemc"
+        url = URI.create("http://cakemc.net:8081/releases")
+        credentials {
+            username = repoUsername
+            password = repoPassword
+        }
+        isAllowInsecureProtocol = true
+    }
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
+val jdkVersion = JavaVersion.VERSION_21
+val jdkVersionString = jdkVersion.toString()
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(jdkVersionString)
+    withSourcesJar()
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.withType<JavaCompile> {
+    sourceCompatibility = jdkVersionString
+    targetCompatibility = jdkVersionString
+    options.encoding = StandardCharsets.UTF_8.toString()
 }
+
+tasks.withType<AbstractArchiveTask> {
+    isReproducibleFileOrder = true
+    isPreserveFileTimestamps = false
+}
+
+tasks.withType<ShadowJar> {
+    configurations = listOf(project.configurations.shadow.get())
+    isZip64 = true
+}
+
+configurations.shadow { isTransitive = false }
+
+publishing {
+    publications.create<MavenPublication>(rootProject.name) {
+        artifact(tasks.shadowJar)
+    }
+    repositories {
+        maven {
+            name = "cakemc"
+            url = URI.create("http://cakemc.net:8081/releases")
+            credentials {
+                username = repoUsername
+                password = repoPassword
+            }
+            isAllowInsecureProtocol = true
+        }
+    }
+}
+
 kotlin {
     jvmToolchain(21)
 }
